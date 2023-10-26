dc77226834498de0237219d46ff19ab92275345b
Aztro-dev <davidperezcastellanos@gmail.com>
2023-10-25 21:20:56 -0500

    Honestly forgot what I changed

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ fn main() {
 
 fn setup(mut commands: Commands) {
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(-7.0, 7.5, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
 
