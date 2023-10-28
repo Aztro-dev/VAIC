f9f54996097df698159646a4e0fd7f317c3d07fb
Aztro-dev <davidperezcastellanos@gmail.com>
2023-10-27 20:24:41 -0500

    Moving objects and snapping rotation and translation

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ use placing::PlacingPlugin;
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::hex("333333").unwrap()))
+        .insert_resource(Msaa::Sample4)
         .add_plugins((
             DefaultPlugins
                 .set(WindowPlugin {
@@ -30,10 +31,14 @@ fn main() {
 }
 
 fn setup(mut commands: Commands) {
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-7.0, 7.5, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(-7.0, 7.5, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+            ..default()
+        },
+        bevy_mod_picking::backends::raycast::RaycastPickCamera::default(),
+        bevy_transform_gizmo::GizmoPickSource::default(),
+    ));
 
     commands.insert_resource(AmbientLight {
         color: Color::WHITE,
--- src/placing.rs
+++ src/placing.rs
@@ -1,14 +1,58 @@
 use bevy::prelude::*;
 use bevy_mod_picking::prelude::*;
+use bevy_transform_gizmo::{GizmoTransformable, TransformGizmoPlugin};
 
 pub struct PlacingPlugin;
 
 impl Plugin for PlacingPlugin {
     fn build(&self, app: &mut App) {
-        app.add_plugins(
+        app.add_plugins((
             DefaultPickingPlugins
                 .build()
                 .disable::<DebugPickingPlugin>(),
+            TransformGizmoPlugin::default(),
+        ))
+        .add_systems(Startup, setup)
+        .add_systems(Update, snap_to_closest);
+    }
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn((
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(StandardMaterial { ..default() }),
+            ..default()
+        },
+        bevy_mod_picking::PickableBundle::default(),
+        bevy_mod_picking::backends::raycast::RaycastPickTarget::default(),
+        GizmoTransformable,
+    ));
+}
+
+const SNAP_DIST: f32 = 0.3;
+const SNAP_ROT: f32 = std::f32::consts::PI / 24.0;
+
+fn snap_to_closest(mut movable_query: Query<&mut Transform, With<GizmoTransformable>>) {
+    for mut transform in movable_query.iter_mut() {
+        transform.translation.x = transform.translation.x - transform.translation.x % SNAP_DIST;
+        transform.translation.y = transform.translation.y - transform.translation.y % SNAP_DIST;
+        transform.translation.z = transform.translation.z - transform.translation.z % SNAP_DIST;
+        let to_euler = transform.rotation.to_euler(EulerRot::XYZ);
+        transform.rotation = Quat::from_euler(
+            EulerRot::XYZ,
+            to_euler.0 - to_euler.0 % SNAP_ROT,
+            to_euler.1 - to_euler.1 % SNAP_ROT,
+            to_euler.2 - to_euler.2 % SNAP_ROT,
+        );
+        println!(
+            "{}, {:?}",
+            transform.translation,
+            transform.rotation.to_euler(EulerRot::XYZ)
         );
     }
 }
