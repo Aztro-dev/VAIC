270e0916e92893838faa1d5916f81a18edaa794c
Aztro-dev <davidperezcastellanos@gmail.com>
2023-11-04 09:11:53 -0500

    [Small change] Unnecessary commented out code

--- src/movement.rs
+++ src/movement.rs
@@ -14,16 +14,6 @@ impl Plugin for MovementPlugin {
 }
 
 fn spawn_camera(mut commands: Commands) {
-    // commands.spawn((
-    //     Camera3dBundle {
-    //         transform: Transform::from_xyz(-7.0, 7.5, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
-    //         ..default()
-    //     },
-    //     bevy_mod_picking::backends::raycast::RaycastPickCamera::default(),
-    //     bevy_transform_gizmo::GizmoPickSource::default(),
-    //     FlyCam,
-    // ));
-
     commands.spawn(Camera3dBundle::default()).insert((
         bevy_mod_picking::backends::raycast::RaycastPickCamera::default(),
         bevy_transform_gizmo::GizmoPickSource::default(),
