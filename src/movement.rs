bf380ce37fee2fa167316452b4f4d58029d7ff1b
Aztro-dev <davidperezcastellanos@gmail.com>
2024-07-02 10:56:45 -0500

    Included CAM Movement but like fr

--- src/movement.rs
+++ src/movement.rs
@@ -1,3 +1,4 @@
+use bevy::input::mouse::MouseMotion;
 use bevy::pbr::ClusterConfig;
 use bevy::prelude::*;
 use bevy_editor_cam::prelude::*;
@@ -11,7 +12,7 @@ impl Plugin for MovementPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(DefaultEditorCamPlugins)
             .add_plugins(bevy_mod_picking::DefaultPickingPlugins)
-            // .add_systems(Update, (switch_control_state, switch_projection))
+            .add_systems(Update, (switch_projection, update_camera))
             .add_systems(Startup, spawn_camera);
     }
 }
@@ -24,26 +25,19 @@ fn spawn_camera(mut commands: Commands, settings: Res<Settings>, asset_server: R
         })
         .insert((
             GridShadowCamera,
-            // OrbitCameraBundle::new(
-            //     OrbitCameraController {
-            //         mouse_rotate_sensitivity: settings.rotate_sensitivity,
-            //         mouse_translate_sensitivity: settings.translate_sensitivity,
-            //         mouse_wheel_zoom_sensitivity: settings.zoom_sensitivity,
-            //         control_state: settings.get_control_state(),
-            //         ..default()
-            //     },
-            //     Vec3::new(-7.0, 7.5, 10.0),
-            //     Vec3::new(0.0, 0.0, 0.0),
-            //     Vec3::Y,
             EditorCam {
                 sensitivity: Sensitivity {
                     orbit: settings.rotate_sensitivity,
                     zoom: settings.zoom_sensitivity,
                 },
+                enabled_motion: EnabledMotion {
+                    pan: false,
+                    ..default()
+                },
                 ..default()
-            }, // Step 2: add camera controller component to any cameras
+            },
+            // Doesn't work on my machine for some reason
             EnvironmentMapLight {
-                // Unrelated to camera controller, needed for lighting:
                 intensity: 1000.0,
                 diffuse_map: asset_server.load("environment_maps/diffuse_rgb9e5_zstd.ktx2"),
                 specular_map: asset_server.load("environment_maps/specular_rgb9e5_zstd.ktx2"),
@@ -66,6 +60,30 @@ fn switch_control_state(
         settings.set_control_state(controller.control_state);
     }
 }
+*/
+
+fn update_camera(
+    mut editor_camera: Query<&mut EditorCam>,
+    keyboard: Res<ButtonInput<KeyCode>>,
+    mut mouse_events: EventReader<MouseMotion>,
+    settings: Res<Settings>,
+) {
+    let mut editor_camera = editor_camera.get_single_mut().unwrap();
+    let view_space_anchor = editor_camera.anchor_view_space();
+    if keyboard.pressed(KeyCode::ShiftLeft) {
+        editor_camera.enabled_motion.pan = true;
+        editor_camera.start_pan(view_space_anchor);
+        for event in mouse_events.read() {
+            let mut movement = event.delta * 100.0;
+            movement.x *= settings.translate_sensitivity.x;
+            movement.y *= settings.translate_sensitivity.y;
+
+            editor_camera.send_screenspace_input(movement);
+        }
+        editor_camera.end_move();
+        editor_camera.enabled_motion.pan = false;
+    }
+}
 
 fn switch_projection(
     mut camera_query: Query<&mut Projection, With<Camera3d>>,
@@ -84,4 +102,4 @@ fn switch_projection(
             Projection::Orthographic(_) => Projection::default(),
         }
     }
-}*/
+}
